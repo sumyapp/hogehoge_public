4b4c706e12b717d15c872722e2497c6f78414e6f
Koichiro Sumi <koichiro.sumi@actcat.co.jp>
2015-10-29 14:31:37 +0900

    copy dirty code from private hogehoge

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,26 @@
+class ApplicationController < ActionController::Base
+  # protect_from_forgery with: :exception
+  def index3
+    "test"
+  end
+  def index2
+  end
+  def index4
+  end
+  def index5
+  end
+  def index
+  end
+  def tetete
+  end
+private
+
+  def waste_action
+    p "test"
+    return true
+  end
+
+  def some_action
+  end
+
+end
